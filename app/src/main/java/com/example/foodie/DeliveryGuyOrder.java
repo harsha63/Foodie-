66e8882e8a648cbabdeddc0aa8b751c631ee6324
saumya gangrade <saumyagangrade8@gmail.com>
2020-04-20 01:34:15 +0530

    Java file added for DeliveryGuy. Need to integrate with MenuList later. Order button in Menu required

---
+++ app/src/main/java/com/example/foodie/DeliveryGuyOrder.java
@@ -0,0 +1,168 @@
+package com.example.foodie;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.internal.$Gson$Preconditions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DeliveryGuyOrder extends AppCompatActivity {
+    private static final String TAG= "Delivery ORDER";
+    RecyclerView recyclerView;
+    OrderAdapter adapter;
+    List<Order>  ordersList,orderTemporary;
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    double lat1,long1,lat2,long2, Radius;
+    ArrayList<Double>latitude = new ArrayList<>();
+    ArrayList<Double>longitude = new ArrayList<>();
+    String Cust, DeliveryUser;
+    Order orders;
+    int count = 0;
+    int count1 = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deliveryboy_orders);
+        fStore = FirebaseFirestore.getInstance();
+        ordersList = new ArrayList<>();
+        orderTemporary = new ArrayList<>();
+        recyclerView = findViewById(R.id.driverOrders);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DeliveryUser = FirebaseAuth.getInstance().getUid();
+        if(ordersList.size()>0){
+            ordersList.clear();
+        }
+        DocumentReference doc = fStore.collection("Customers").document(DeliveryUser);
+        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot f = task.getResult();
+                lat2 = f.getDouble("Latitude");
+                long2 = f.getDouble("Longitude");
+                Radius = f.getDouble("Radius");
+                Log.d(TAG,"But, the secondary value is: "+lat2+" "+long2);
+            }
+        });
+        fStore.collection("Orders").whereEqualTo("Assigned",false).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (DocumentSnapshot documentSnapshot : task.getResult()) {
+                    orders = new Order(documentSnapshot.getString("UserID"), documentSnapshot.getString("Restaurant"), documentSnapshot.getString("UserName"),
+                                    documentSnapshot.getString("UserPhone"), documentSnapshot.getDouble("Price")/*,
+                                    documentSnapshot.getBoolean("Assigned")*/);
+                    orderTemporary.add(orders);
+                    Log.d(TAG,"Order object name: "+orders.getuName());
+                    String Rid = documentSnapshot.getString("RestroID");
+                    DocumentReference documentReference = fStore.collection("Restaurants").document(Rid);
+                    documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    lat1 = document.getDouble("Latitude");
+                                    latitude.add(lat1);
+                                    long1 = document.getDouble("Longitude");
+                                    longitude.add(long1);
+                                    Log.d(TAG,"But, the initial value is: "+lat1+" "+long1);
+                                    count++;
+
+                                    final String restuuu = document.getString("Name");
+                                    DocumentReference doc = fStore.collection("Customers").document(DeliveryUser);
+                                    doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot docu = task.getResult();
+                                                if (docu.exists()) {
+
+                                                    String name = docu.getString("fName");
+                                                    Location RestLoc = new Location("Rest");
+                                                    RestLoc.setLatitude(latitude.get(count1));
+                                                    RestLoc.setLongitude(longitude.get(count1));
+                                                    Location CustLoc = new Location("Cust");
+                                                    CustLoc.setLatitude(lat2);
+                                                    CustLoc.setLongitude(long2);
+
+                                                    Log.d(TAG, "The distance that comes out is " + CustLoc.distanceTo(RestLoc) / 1000 + " Yes. " + count1 + " " + orders.getRest());
+
+
+                                                    if (CustLoc.distanceTo(RestLoc) / 1000 < Radius) {
+                                                        ordersList.add(orderTemporary.get(count1));
+                                                        //Log.d(TAG, "The distance that comes out is " + CustLoc.distanceTo(RestLoc) / 1000 + " Yes. " + count1 + " " + orders.getRest());
+                                                        //Log.d(TAG,long1+" "+lat1);
+
+                                                    }
+                                                    count1++;
+                                                    if(count1==count) {
+                                                        Log.d(TAG,"We are in the endgame");
+                                                        adapter = new OrderAdapter(DeliveryGuyOrder.this, ordersList);
+                                                        recyclerView.setAdapter(adapter);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    Log.d(TAG, "No such document");
+                                }
+                            } else {
+                                Log.d(TAG, "get failed with ", task.getException());
+                            }
+                        }
+                    });
+                    /*     if(documentSnapshot.getBoolean("Assigned") == false){
+                                ordersList.add(orders);
+                            }*/
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(DeliveryGuyOrder.this, "failed :(", Toast.LENGTH_SHORT).show();
+                 Log.v("Failed", e.getMessage());
+            }
+        });
+
+
+
+    }
+
+
+
+    public static void wait(int ms){
+        try {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/foodie/OrderAdapter.java
@@ -0,0 +1,158 @@
+package com.example.foodie;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firestore.v1.StructuredQuery;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class OrderAdapter extends  RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
+    private Context context;
+    DeliveryGuyOrder driverOrders;
+    private List<Order> ordersList ;
+    FirebaseAuth mFAuth;
+    FirebaseFirestore fStore;
+    String Email;
+    String OTP;
+    Order od;
+
+    public OrderAdapter(Context context, List<Order> ordersList) {
+        this.context = context;
+        this.ordersList = ordersList;
+    }
+    public static int generateRandomDigits() {
+        int m = (int) Math.pow(10, 3);
+        return m + new Random().nextInt(9 * m);
+    }
+
+    public void enableStrictMode()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+    }
+    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.delivery_order_card, null, false);
+        OrderViewHolder orderViewHolder = new OrderViewHolder(view);
+        return orderViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderViewHolder holder, final int position) {
+        Order orders = ordersList.get(position);
+        holder.textViewRest.setText(orders.getRest());
+        holder.textViewuname.setText(orders.getuName());
+        holder.textViewPhone.setText(orders.getuPhone());
+
+        holder.accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFAuth = FirebaseAuth.getInstance();
+                fStore = FirebaseFirestore.getInstance();
+                final String userId = mFAuth.getUid();
+                od = ordersList.get(position);
+                DocumentReference documentReference = fStore.collection("Orders").document(od.getId());
+                Map<String,Object> user = new HashMap<>();
+                user.put("DeliveryId",userId);
+                user.put("Assigned",true);
+                documentReference.set(user, SetOptions.merge());
+                documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+
+                                String Uid = document.getString("UserID");
+                                DocumentReference doc = fStore.collection("Customers").document(Uid);
+                                doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful()){
+                                            DocumentSnapshot User = task.getResult();
+                                            if(User.exists()){
+                                                Email = User.getString("email");
+                                                OTP = String.valueOf(generateRandomDigits());
+
+                                                try{
+
+                                                    enableStrictMode();
+                                                    //Mailer.send("obliviousalwaysforever@gmail.com","wha1sup??",Email,"Order OTP",OTP+" Is yout OTP. Do you understand?");
+
+                                                }
+                                                catch(Exception e){
+                                                    System.out.println(e.toString()+ "  Hallelujah");
+                                                }
+                                                DocumentReference documentReference = fStore.collection("Orders").document(od.getId());
+                                                Map<String,Object> users = new HashMap<>();
+                                                users.put("OTP",OTP);
+                                                users.put("Status",2);
+                                                documentReference.set(users, SetOptions.merge());
+
+                                            }
+                                        }
+                                    }
+                                });
+
+
+                            } else {
+
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+
+
+            }
+        });
+        /*     holder.textViewPrice.setText((int) orders.getAmount());*/
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return ordersList.size();
+    }
+
+
+    class OrderViewHolder extends RecyclerView.ViewHolder{
+        TextView textViewRest, textViewuname, textViewPhone/*, textViewPrice*/;
+        Button decline, accept;
+
+
+        public OrderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewRest = itemView.findViewById(R.id.RestroName);
+            textViewuname = itemView.findViewById(R.id.custName);
+            textViewPhone = itemView.findViewById(R.id.custPhone);
+//            decline = itemView.findViewById(R.id.decline);
+            accept = itemView.findViewById(R.id.accept);
+            /*            textViewPrice = itemView.findViewById(R.id.textViewPrice);*/
+        }
+    }
+}
